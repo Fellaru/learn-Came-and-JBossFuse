5e09edc8055cd02f80c323609e4c972c167f5d1e
Ella Fischenko <efischenko@cinimex.ru>
2017-06-09 17:05:39 +0300

    CustomerWSDL

---
+++ customerWS/src/main/java/ru/cinimex/learn/customer/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://learn.cinimex.ru/customer")
+package ru.cinimex.learn.customer;
---
+++ customerWS/src/main/java/ru/cinimex/learn/service/CustomerWsIm.java
@@ -0,0 +1,19 @@
+package ru.cinimex.learn.service;
+
+import ru.cinimex.learn.customer.CustomerBDlist;
+import ru.cinimex.learn.customer.CustomerType;
+import ru.cinimex.learn.customer.Customers;
+import ru.cinimex.learn.customer.PhoneList;
+
+/**
+ * Created by efischenko on 09.06.2017.
+ */
+public class CustomerWsIm implements CustomerWS {
+    public CustomerBDlist getCustomerByPhone(PhoneList phone) {
+        return null;
+    }
+
+    public Customers createCustomer(Customers dateForCustomer) {
+
+    }
+}
