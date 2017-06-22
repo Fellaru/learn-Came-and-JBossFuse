29f2f864b08f0ebc87c278522ffd1b25e6596e4b
Ella Fischenko <efischenko@cinimex.ru>
2017-06-22 19:54:56 +0300

    GetCustByPhone first edition

--- project2/Route/src/main/java/ru/cinimex/learn/GetCustByPhone.java
+++ project2/Route/src/main/java/ru/cinimex/learn/GetCustByPhone.java
@@ -7,43 +7,60 @@ import ru.cinimex.learn.customerws.customer.Customers;
 import ru.cinimex.learn.customerws.customer.PhoneList;
 import ru.cinimex.learn.dao.ICustomerDao;
 
-import java.util.Iterator;
 import java.util.List;
 
 
 /**
  * Created by efischenko on 20.06.2017.
  */
-public class GetCustByPhone implements Processor{
+public class GetCustByPhone implements Processor {
     public ICustomerDao iCustomerDao;
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        Object[] args = exchange.getIn().getBody(Object[].class);
-        exchange.getOut().setBody( new Customers());
-    }
-
-}
-/*
+    private List<CustomerType> customers;
 
     @Override
     public void process(Exchange exchange) throws Exception {
         Customers dateForCustomer = new Customers();
-        List<CustomerType> customers = dateForCustomer.getCustomers();
+        customers = dateForCustomer.getCustomers();
 
         Object[] args = exchange.getIn().getBody(Object[].class);
         List<Long> phones = ((PhoneList) args[0]).getPhones();
+        for (Long p : phones) {
+            getCustomer(p);
+        }
+        exchange.getOut().setBody(dateForCustomer);
+    }
 
-        Iterator<Long> iterator = phones.iterator();
+
+    public void getCustomer(Long phone) {
+        /*iCustomerDao.getByPhone(phone);*/
+        //TODO Почему возвращает NullPointerException
+        //TODO Сделать конвертер который бы возвращал CustomerType
+        CustomerType customerType = new CustomerType();
+        customerType.setFirstName("Заглушка");
+        customerType.setLastName("Заглушка");
+        customers.add(customerType);
+    }
+
+    public ICustomerDao getiCustomerDao() {
+        return iCustomerDao;
+    }
+
+    public void setiCustomerDao(ICustomerDao iCustomerDao) {
+        this.iCustomerDao = iCustomerDao;
+    }
+
+
+}
+
+
+/* phones.forEach(c -> getCustomer(c));*/
+//TODO разобраться почему тут не работает
+
+
+       /* Iterator<Long> iterator = phones.iterator();
         while (iterator.hasNext()){
             Long phone = iterator.next();
-            iCustomerDao.getByPhone(phone);
-            CustomerType customerType = new CustomerType();
-            customerType.setFirstName("Заглушка");
-            customerType.setLastName("Заглушка");
-            customers.add(customerType);
-        }
-        exchange.getOut().setBody( new Customers());
-    }
-*/
+            getCustomer(phone);
 
-
+        }*/
+//TODO Один не верный телефон и сразу весь ответ неверный! Возможно ли сделать через SoapFault ошибку только у одного customera
--- project2/Route/src/main/java/ru/cinimex/learn/MyRoute.java
+++ project2/Route/src/main/java/ru/cinimex/learn/MyRoute.java
@@ -7,15 +7,7 @@ import ru.cinimex.learn.dao.ICustomerDao;
  * Created by efischenko on 20.06.2017.
  */
 public class MyRoute extends RouteBuilder {
-   public ICustomerDao iCustomerDao;
 
-    public ICustomerDao getiCustomerDao() {
-        return iCustomerDao;
-    }
-
-    public void setiCustomerDao(ICustomerDao iCustomerDao) {
-        this.iCustomerDao = iCustomerDao;
-    }
 
     @Override
     public void configure() throws Exception {
