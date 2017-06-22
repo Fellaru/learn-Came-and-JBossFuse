9fc84b8d8379cb0a25cb466aba153cd7a6f834b2
Ella Fischenko <efischenko@cinimex.ru>
2017-06-22 15:13:09 +0300

    Maybe application works or maybe it does'nt

--- project2/Route/src/main/java/ru/cinimex/learn/GetCustByPhone.java
+++ project2/Route/src/main/java/ru/cinimex/learn/GetCustByPhone.java
@@ -5,12 +5,10 @@ import org.apache.camel.Processor;
 import ru.cinimex.learn.customerws.customer.Customers;
 
 
-
 /**
  * Created by efischenko on 20.06.2017.
  */
 public class GetCustByPhone implements Processor{
-
     @Override
     public void process(Exchange exchange) throws Exception {
         Object[] args = exchange.getIn().getBody(Object[].class);
--- project2/Route/src/main/java/ru/cinimex/learn/MyRoute.java
+++ project2/Route/src/main/java/ru/cinimex/learn/MyRoute.java
@@ -1,12 +1,21 @@
 package ru.cinimex.learn;
 
 import org.apache.camel.builder.RouteBuilder;
-
+import ru.cinimex.learn.dao.ICustomerDao;
 
 /**
  * Created by efischenko on 20.06.2017.
  */
 public class MyRoute extends RouteBuilder {
+   public ICustomerDao iCustomerDao;
+
+    public ICustomerDao getiCustomerDao() {
+        return iCustomerDao;
+    }
+
+    public void setiCustomerDao(ICustomerDao iCustomerDao) {
+        this.iCustomerDao = iCustomerDao;
+    }
 
     @Override
     public void configure() throws Exception {
