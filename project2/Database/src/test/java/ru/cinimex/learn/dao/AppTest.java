e9194e9e911ea1ffaf396c3a8e6d446fc121d815
Ella Fischenko <efischenko@cinimex.ru>
2017-06-23 16:40:08 +0300

    Add parent pom

--- project2/Database/src/test/java/ru/cinimex/learn/dao/AppTest.java
+++ project2/Database/src/test/java/ru/cinimex/learn/dao/AppTest.java
@@ -10,21 +10,21 @@ import java.util.Random;
 
 
 public class AppTest extends TestCase {
-	private String firstName = "Ella";
-	private String lastName = "Fischenko";
-	private Long phone =  new Random().nextLong();
+    private String firstName = "Ella";
+    private String lastName = "Fischenko";
+    private Long phone =  new Random().nextLong();
 
 
-	public void testApp() {
-		SessionFactory sessionFactory = new Configuration().configure()
-				.buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+    public void testApp() {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
 
-		Customer user = new Customer().setFirstName(firstName).setLastName(lastName).setPhone(phone);
-		session.saveOrUpdate(user);
+        Customer user = new Customer().setFirstName(firstName).setLastName(lastName).setPhone(phone);
+        session.saveOrUpdate(user);
 
-		session.getTransaction().commit();
-		session.close();
-	}
+        session.getTransaction().commit();
+        session.close();
+    }
 }
