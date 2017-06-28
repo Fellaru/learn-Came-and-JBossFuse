8ed9a091ce51d0568092416728d739ec6158cc34
Ella Fischenko <efischenko@cinimex.ru>
2017-06-28 18:10:13 +0300

    fight with Uppercase 2

---
+++ project2/route/src/main/java/ru/cinimex/learn/MyRoute.java
@@ -0,0 +1,41 @@
+package ru.cinimex.learn;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+
+
+/**
+ * Created by efischenko on 20.06.2017.
+ */
+public class MyRoute extends RouteBuilder {
+
+
+
+
+    @Override
+    public void configure() throws Exception {
+        from("cxf:bean:customer_ws")
+                .log(LoggingLevel.INFO,CreateCustomer.class.getName(),"Start route")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        String abc = "ads";
+                    }
+                })
+                .choice()
+                .when()
+                    .simple("${in.header.operationName} == 'createCustomer'")
+                        .log(LoggingLevel.INFO,CreateCustomer.class.getName(),"Start CreateCustomer")
+                        .process(new CreateCustomer())
+                .when()
+                .simple("${in.header.operationName} == 'getCustomerByPhone'")
+                         .log(LoggingLevel.INFO,CreateCustomer.class.getName(),"Start GetCustByPhone").process(new GetCustByPhone())
+                .endChoice();
+                /*.to("direct-vm:phonebyfio");*/
+
+       /* from("direct-vm:phonebyfio").to("activemq:GETPHONE.INPUT");*/
+    }
+}
